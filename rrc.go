1228e7858b98ed5b5adcf047216dd8f738ba12b9
tylhjy <42135781+tylhjy@users.noreply.github.com>
2021-09-27 13:56:15 +0800

    Update rrc.go

--- rrc.go
+++ rrc.go
@@ -66,7 +66,7 @@ func main() {
 	if err == nil {
 		TransitNode = conf.TransitHost
 		if len(os.Args) < 3 {
-			log.Fatal("Please input host and path that you want to tail, e.g. rail 192.168.2.1 D://456.txt")
+			log.Fatal("Please input host and path that you want to tail, e.g. rrc 192.168.2.1 cat D://test.txt")
 		}
 	} else {
 		fmt.Println("Please config conf.yaml!")
